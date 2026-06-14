c6952d5bc3d4a498c259edeffe9619515c45bf20
ᗷᗩᔕ_ᑭOᗪ <163411670+Baspod@users.noreply.github.com>
2026-06-14 17:36:29 +0300

    Delete plugins directory

--- plugins/dummy_source.py
+++
@@ -1,7 +0,0 @@
-import asyncio
-class DummySource:
-    def __init__(self, **kwargs):
-        pass
-    async def start(self, bus):
-        print("[SYSTEM] Running in DUMMY mode. No data source connected.")
-        await asyncio.Event().wait()
